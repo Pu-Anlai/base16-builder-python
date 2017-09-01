9bf3c9ed5b2fa0e0f2e3a3e352aebfc5febaf23c
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-09-01 12:40:52 +0200

    Clean up path resolution

--- builder.py
+++ builder.py
@@ -8,6 +8,11 @@ from queue import Queue
 CWD = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
+def rel_to_cwd(*args):
+    """Get absolute real path of $path with $CWD as base."""
+    return os.path.join(CWD, *args)
+
+
 def get_yaml_dict(yaml_file):
     with open(yaml_file, 'r') as f:
         yaml_dict = yaml.safe_load(f.read())
@@ -21,7 +26,7 @@ def download_from_yaml(yaml_file, base_dir):
     yaml_dict = get_yaml_dict(yaml_file)
     job_list = []
     for key, value in yaml_dict.items():
-        job_list.append((value, os.path.join(CWD, base_dir, key)))
+        job_list.append((value, rel_to_cwd(base_dir, key)))
 
     git_clone_all(job_list)
 
@@ -33,6 +38,7 @@ def git_clone(git_url, path):
         shutil.rmtree(path)
 
     os.makedirs(path, exist_ok=True)
+    print('Start cloning from {}…'.format(git_url))
     git_proc = subprocess.run(['git', 'clone', git_url, path],
                               stderr=subprocess.PIPE)
     if git_proc.returncode == 0:
@@ -44,6 +50,8 @@ def git_clone(git_url, path):
 
 
 def git_clone_worker(queue):
+    """Worker thread for picking up git clone jobs from $queue until it
+    receives None."""
     while True:
         job = queue.get()
         if job is None:
@@ -61,7 +69,7 @@ def git_clone_all(job_list):
         queue.put(job)
 
     threads = []
-    for _ in range(50):
+    for _ in range(40):
         thread = Thread(target=git_clone_worker, args=(queue, ))
         thread.start()
         threads.append(thread)
@@ -77,11 +85,12 @@ def git_clone_all(job_list):
 
 def update():
     print('Cloning sources…')
-    download_from_yaml(os.path.join(CWD, 'sources.yaml'),
-                       os.path.join(CWD, 'sources'))
+    download_from_yaml(rel_to_cwd('sources.yaml'),
+                       rel_to_cwd('sources'))
     print('Cloning templates…')
-    download_from_yaml(os.path.join(CWD, 'sources', 'templates', 'list.yaml'),
-                       os.path.join(CWD, 'templates'))
+    download_from_yaml(rel_to_cwd('sources', 'templates', 'list.yaml'),
+                       rel_to_cwd('templates'))
     print('Cloning schemes…')
-    download_from_yaml(os.path.join(CWD, 'sources', 'schemes', 'list.yaml'),
-                       os.path.join(CWD, 'schemes'))
+    download_from_yaml(rel_to_cwd('sources', 'schemes', 'list.yaml'),
+                       rel_to_cwd('schemes'))
+    print('Completed updating repositories.')
