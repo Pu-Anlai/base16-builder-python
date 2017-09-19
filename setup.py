7c0f581bafdcf79932a37fa9f857d1106ba92554
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-09-19 16:47:03 +0200

    Initial package structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+      name='pybase16-builder',
+      version='0.1',
+      description='A base16 colorscheme buider for Python',
+      long_description=open('README.md').read(),
+      url='https://github.com/InspectorMustache/pybase16-builder',
+      packages=['pybase16-builder'],
+      author='Pu Anlai',
+      license='MIT',
+      classifiers=[
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: End Users/Desktop',
+                   'Topic :: Other/Nonlisted Topic',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3 :: Only'
+                  ],
+      keywords='base16'
+     )
