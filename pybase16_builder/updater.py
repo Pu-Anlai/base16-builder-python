d21d5adedfeba75ba283ceca0af51c300b8f7341
Pu Anlai <inspectormustache@users.noreply.github.com>
2019-09-09 21:31:08 +1000

    Switch updater from threads to asyncio

--- pybase16_builder/updater.py
+++ pybase16_builder/updater.py
@@ -1,11 +1,8 @@
 import os
 import sys
-import subprocess
 import shutil
-import threading
-from queue import Queue
-from .shared import (get_yaml_dict, rel_to_cwd,
-                     JobOptions, verb_msg, thread_print)
+import asyncio
+from .shared import get_yaml_dict, rel_to_cwd, verb_msg
 
 
 def write_sources_file():
@@ -21,86 +18,39 @@ def write_sources_file():
         file_.write(file_content)
 
 
-def initiate_job_options(**kwargs):
-    """Return a JobOptions object."""
-    return JobOptions(**kwargs)
-
-
-def yaml_to_queue(yaml_file, base_dir):
-    """Return a queues consisting of jobs made up of git repos from $yaml_file
-    as well as their base target directory."""
-    yaml_dict = get_yaml_dict(yaml_file)
-    queue = Queue()
-    for key, value in yaml_dict.items():
-        queue.put((value, rel_to_cwd(base_dir, key)))
-
-    return queue
-
-
-def git_clone(git_url, path, job_options):
+async def git_clone(git_url, path, verbose=False):
     """Clone git repository at $git_url to $path."""
+    if verbose:
+        print('Cloning {}...'.format(git_url))
     if os.path.exists(os.path.join(path, '.git')):
         # get rid of local repo if it already exists
         shutil.rmtree(path)
 
     os.makedirs(path, exist_ok=True)
-    thread_print(job_options.lock, 'Start cloning from {}…'.format(git_url),
-                 verbose=job_options.verbose)
-    my_env = os.environ.copy()
-    my_env['GIT_TERMINAL_PROMPT'] = '0'
-    git_proc = subprocess.Popen(['git', 'clone', git_url, path],
-                                stderr=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                env=my_env)
+    git_proc = await asyncio.create_subprocess_exec(
+        'git', 'clone', git_url, path,
+        stderr=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE)
+    stdout, stderr = await git_proc.communicate()
 
-    try:
-        stdoutmsg, stderrmsg = git_proc.communicate(timeout=120)
-    except subprocess.TimeoutExpired:
-        git_proc.kill()
-        stderrmsg = b'Timed out.'
-
-    if git_proc.returncode == 0:
-        thread_print(job_options.lock, 'Cloned {}.'.format(git_url),
-                     verbose=job_options.verbose)
-    else:
-        err_msg = verb_msg(
-            '{}:\n{}'.format(git_url, stderrmsg.decode('utf-8')), lvl=2)
-        thread_print(job_options.lock, err_msg)
+    if git_proc.returncode != 0:
+        print(verb_msg(
+            '{}:\n'.format(git_url, stderr.decode('utf-8'))))
+    elif verbose:
+        print('Cloned {}'.format(git_url))
 
 
-def git_clone_worker(queue, job_options):
-    """Worker thread for picking up git clone jobs from $queue until it
-    receives None."""
-    while True:
-        job = queue.get()
-        if job is None:
-            break
-        git_url, path = job
-        git_clone(git_url, path, job_options)
-        queue.task_done()
+async def git_clone_scheduler(yaml_file, base_dir, verbose=False):
+    """Create task list for clone jobs and run them asynchronously."""
+    jobs = generate_jobs_from_yaml(yaml_file, base_dir)
+    task_list = [git_clone(*args_, verbose=verbose) for args_ in jobs]
+    await asyncio.gather(*task_list)
 
 
-def git_clone_from_job_options(queue, job_options):
-    """Deal with all git clone jobs on $queue."""
-    if queue.qsize() < 20:
-        thread_num = queue.qsize()
-    else:
-        thread_num = 20
-
-    threads = []
-    for _ in range(thread_num):
-        thread = threading.Thread(target=git_clone_worker,
-                                  args=(queue, job_options))
-        thread.start()
-        threads.append(thread)
-
-    queue.join()
-
-    for _ in range(thread_num):
-        queue.put(None)
-
-    for thread in threads:
-        thread.join()
+def generate_jobs_from_yaml(yaml_file, base_dir):
+    yaml_dict = get_yaml_dict(yaml_file)
+    for key, value in yaml_dict.items():
+        yield (value, rel_to_cwd(base_dir, key))
 
 
 def update(custom_sources=False, verbose=False):
@@ -109,25 +59,28 @@ def update(custom_sources=False, verbose=False):
         print('Git executable not found in $PATH.')
         sys.exit(1)
 
+    event_loop = asyncio.get_event_loop()
+
     if not custom_sources:
         print('Creating sources.yaml…')
         write_sources_file()
         print('Cloning sources…')
         sources_file = rel_to_cwd('sources.yaml')
-        queue = yaml_to_queue(sources_file, rel_to_cwd('sources'))
-        job_options = initiate_job_options(verbose=verbose)
-        git_clone_from_job_options(queue, job_options)
+        event_loop.run_until_complete(
+            git_clone_scheduler(sources_file,
+                                rel_to_cwd('sources'),
+                                verbose=verbose))
 
     print('Cloning templates…')
-    queue = yaml_to_queue(rel_to_cwd('sources', 'templates', 'list.yaml'),
-                          rel_to_cwd('templates'))
-    job_options = initiate_job_options(verbose=verbose)
-    git_clone_from_job_options(queue, job_options)
+    event_loop.run_until_complete(git_clone_scheduler(
+        rel_to_cwd('sources', 'templates', 'list.yaml'),
+        rel_to_cwd('templates'),
+        verbose=verbose))
 
     print('Cloning schemes…')
-    queue = yaml_to_queue(rel_to_cwd('sources', 'schemes', 'list.yaml'),
-                          rel_to_cwd('schemes'))
-    job_options = initiate_job_options(verbose=verbose)
+    event_loop.run_until_complete(git_clone_scheduler(
+        rel_to_cwd('sources', 'schemes', 'list.yaml'),
+        rel_to_cwd('schemes'),
+        verbose=verbose))
 
-    git_clone_from_job_options(queue, job_options)
-    print('Completed updating repositories.')
+    event_loop.close()
