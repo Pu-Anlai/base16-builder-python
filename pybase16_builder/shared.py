cfa2364fa641e7c81a3208a75e86f61b628f4184
井宇飞 <A137294381b@163.com>
2021-05-09 11:03:19 +0800

    fix gbk encoding error

--- pybase16_builder/shared.py
+++ pybase16_builder/shared.py
@@ -45,7 +45,7 @@ def get_yaml_dict(yaml_file):
     """Return a yaml_dict from reading yaml_file. If yaml_file is empty or
     doesn't exist, return an empty dict instead."""
     try:
-        with open(yaml_file, "r") as file_:
+        with open(yaml_file, "r", encoding="utf8") as file_:
             yaml_dict = yaml.safe_load(file_.read()) or {}
         return yaml_dict
     except FileNotFoundError:
